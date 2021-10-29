b3077e97ba1514cb652b3a8282286d0317938744
Karina Duran Munhos <63967857+KarinaDuran@users.noreply.github.com>
2021-10-29 13:10:02 -0300

    Update PCB.java

--- src/kernel/PCB.java
+++ src/kernel/PCB.java
@@ -1,7 +1,7 @@
 package kernel;
 import operacoes.Operacao;
 
-public class PCB {
+public class PCB implements Comparable<PCB>{
 	public enum Estado {NOVO, PRONTO, EXECUTANDO, ESPERANDO, TERMINADO};
 	public int idProcesso;
 	public Estado estado = Estado.NOVO;
@@ -13,9 +13,11 @@ public class PCB {
 
 	
 	public int compareTo(PCB outro){
-		if(outro.Chute > this.Chute || (outro.Chute == this.Chute && this.idProcesso < outro.idProcesso ))
-		return 1;
-		else return 0;
+		//if(outro.Chute > this.Chute || (outro.Chute == this.Chute && this.idProcesso < outro.idProcesso ))
+		//return 1;
+		//else return 0;
+		if(outro.momentoPronto > this.momentoPronto || (outro.momentoPronto == this.momentoPront && outro.idProcesso>this.idProcesso)) return 1
+			else return 0;
 	}
 
 }
